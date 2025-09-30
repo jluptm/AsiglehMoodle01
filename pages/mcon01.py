0adeed33c0b8435d167d7dede4f9f2852f9992d2
jluptm <jl.uptm@gmail.com>
2025-09-30 07:44:12 -0400

    Update mcon01.py

--- pages/mcon01.py
+++ pages/mcon01.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 # Configuración de la conexión a la base de datos
 DB_CONFIG = {
-    'host': '185.214.132.9:3300',
+    'host': '185.214.132.9',
     'user': 'pjluis1010@gmail.com',
     'password': '>BM#d|PAFV7v',
     'database': 'u548971155_srHVY'}
@@ -44,6 +44,7 @@ df
 #         else:
 
 #             st.write("No hay datos para este cuestionario.")
+
 
 
 
